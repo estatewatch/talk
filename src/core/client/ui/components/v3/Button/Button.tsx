08d8b33fb71ff5c6b520f72d4985979b2cf6e84f
nick-funk <nick.funk@outlook.com>
2020-04-01 15:11:59 -0600

    The Lord of the Buttons: Return of the Buttons

--- src/core/client/ui/components/v3/Button/Button.tsx
+++ src/core/client/ui/components/v3/Button/Button.tsx
@@ -5,6 +5,7 @@ import React from "react";
 import { BaseButton } from "coral-ui/components/v2";
 import { BaseButtonProps } from "coral-ui/components/v2/BaseButton";
 import { withStyles } from "coral-ui/hocs";
+import { Omit } from "coral-ui/types";
 
 import styles from "./Button.css";
 
@@ -17,18 +18,12 @@ interface Props extends Omit<BaseButtonProps, "ref"> {
   classes: typeof styles & BaseButtonProps["classes"];
   className?: string;
 
-  fontFamily?: "primary" | "secondary" | "none";
-  fontWeight?: "regular" | "semiBold" | "bold" | "none";
-  fontSize?: "extraSmall" | "small" | "medium" | "large" | "none";
-  textAlign?: "left" | "center" | "right";
-  paddingSize?: "extraSmall" | "small" | "medium" | "large" | "none";
-  color?: "primary" | "secondary" | "positive" | "negative" | "none";
-  variant?: "filled" | "outlined" | "flat" | "none";
+  size?: "medium";
+  color?: "streamBlue";
+  variant?: "filled";
 
   upperCase?: boolean;
-  underline?: boolean;
   disabled?: boolean;
-  fullWidth?: boolean;
 }
 
 export class Button extends React.Component<Props> {
@@ -37,19 +32,12 @@ export class Button extends React.Component<Props> {
       classes,
       className,
       variant = "filled",
-      color = "primary",
-      textAlign = "center",
-      fontSize = "small",
-      fontFamily = "primary",
-      fontWeight = "bold",
-      paddingSize = "small",
+      color = "streamBlue",
+      size = "medium",
       children,
       disabled = false,
       to,
       upperCase = false,
-      underline = false,
-      fullWidth = false,
-      href,
       ...rest
     } = this.props;
 
@@ -58,41 +46,10 @@ export class Button extends React.Component<Props> {
       classes.base,
       {
         [classes.filled]: variant === "filled",
-        [classes.outlined]: variant === "outlined",
-        [classes.flat]: variant === "flat",
-        [classes.fontSizeExtraSmall]: fontSize === "extraSmall",
-        [classes.fontSizeSmall]: fontSize === "small",
-        [classes.fontSizeMedium]: fontSize === "medium",
-        [classes.fontSizeLarge]: fontSize === "large",
-        [classes.textAlignLeft]: textAlign === "left",
-        [classes.textAlignCenter]: textAlign === "center",
-        [classes.textAlignRight]: textAlign === "right",
-        [classes.fontFamilyPrimary]: fontFamily === "primary",
-        [classes.fontFamilySecondary]: fontFamily === "secondary",
-        [classes.fontWeightPrimaryRegular]:
-          fontFamily === "primary" && fontWeight === "regular",
-        [classes.fontWeightPrimarySemiBold]:
-          fontFamily === "primary" && fontWeight === "semiBold",
-        [classes.fontWeightPrimaryBold]:
-          fontFamily === "primary" && fontWeight === "bold",
-        [classes.fontWeightSecondaryRegular]:
-          fontFamily === "secondary" && fontWeight === "regular",
-        [classes.fontWeightSecondarySemiBold]:
-          fontFamily === "secondary" && fontWeight === "semiBold",
-        [classes.fontWeightSecondaryBold]:
-          fontFamily === "secondary" && fontWeight === "bold",
-        [classes.paddingSizeExtraSmall]: paddingSize === "extraSmall",
-        [classes.paddingSizeSmall]: paddingSize === "small",
-        [classes.paddingSizeMedium]: paddingSize === "medium",
-        [classes.paddingSizeLarge]: paddingSize === "large",
-        [classes.colorPrimary]: color === "primary",
-        [classes.colorSecondary]: color === "secondary",
-        [classes.colorPositive]: color === "positive",
-        [classes.colorNegative]: color === "negative",
+        [classes.sizeMedium]: size === "medium",
+        [classes.colorStreamBlue]: color === "streamBlue",
         [classes.disabled]: disabled,
         [classes.upperCase]: upperCase,
-        [classes.underline]: underline,
-        [classes.fullWidth]: fullWidth,
       },
       className
     );
@@ -103,8 +60,6 @@ export class Button extends React.Component<Props> {
         classes={pick(classes, "keyboardFocus", "mouseHover")}
         disabled={disabled}
         to={to}
-        href={href}
-        anchor={href ? true : false}
         {...rest}
       >
         {children}
