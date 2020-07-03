eeb2507d57cbcc402c420bda93b5ed1f4128662b
nick-funk <nick.funk@outlook.com>
2020-07-03 13:25:30 -0600

    Update rejected comment message to v3 callout
    CORL-1155

--- src/core/client/stream/tabs/Comments/Stream/PostCommentForm/PostCommentRejectedMessage.tsx
+++ src/core/client/stream/tabs/Comments/Stream/PostCommentForm/PostCommentRejectedMessage.tsx
@@ -1,12 +1,9 @@
-import cn from "classnames";
 import { Localized } from "@fluent/react/compat";
 import React, { FunctionComponent } from "react";
 
 import CLASSES from "coral-stream/classes";
-import { Flex, Message } from "coral-ui/components/v2";
-import { Button } from "coral-ui/components/v3";
-
-import styles from "./PostCommentRejectedMessage.css";
+import { Icon } from "coral-ui/components/v2";
+import { CallOut } from "coral-ui/components/v3";
 
 export interface PostCommentRejectedProps {
   onDismiss: () => void;
@@ -16,26 +13,18 @@ const PostCommentRejected: FunctionComponent<PostCommentRejectedProps> = (
   props
 ) => {
   return (
-    <Message color="error" className={CLASSES.createComment.rejected} fullWidth>
-      <Flex justifyContent="space-between" alignItems="center" className={styles.flex}>
+    <CallOut
+      className={CLASSES.createComment.rejected}
+      color="negative"
+      titleWeight="semiBold"
+      icon={<Icon size="sm">error</Icon>}
+      title={
         <Localized id="comments-submitStatus-submittedAndRejected">
           <div>This comment has been rejected for violating our guidelines</div>
         </Localized>
-        <div className={styles.buttonWrapper}>
-          <Localized id="comments-submitStatus-dismiss">
-            <Button
-              className={cn(CLASSES.createComment.dismissButton, styles.button)}
-              onClick={props.onDismiss}
-              variant="flat"
-              color="none"
-              underline
-            >
-              Dismiss
-            </Button>
-          </Localized>
-        </div>
-      </Flex>
-    </Message>
+      }
+      onClose={props.onDismiss}
+    />
   );
 };
 
